ac508a4b0dd2be81d2d42816e1371a8cb8b3f214
Nasir Mustapha <teboy.com@gmail.com>
2025-09-10 16:24:49 +0100

    Update run.bat to launch Edge in app mode and comment out kiosk mode

--- run.bat
+++ run.bat
@@ -5,4 +5,5 @@ start /MIN "" python main.py
 timeout /t 5 /nobreak
 
 :: Launch Edge in fullscreen kiosk mode
-start "" "msedge" --kiosk --edge-kiosk-type=fullscreen --no-first-run --disable-features=msEdgeEnterpriseModePolicies http://127.0.0.1:8000/index.html
+@REM start "" "msedge" --kiosk --edge-kiosk-type=fullscreen --no-first-run --disable-features=msEdgeEnterpriseModePolicies http://127.0.0.1:8000/index.html
+start "" "msedge" --app=http://127.0.0.1:8000/index.html
